ea6be36028fa129832a6bfc82d69e7f63d870cdc
Gregory Grant <gregorygrant@MacBookPro.lan>
2026-06-04 10:13:14 -0400

    comment out note in payment

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -252,11 +252,11 @@ export default function PaymentPage() {
             <svg className="w-5 h-5 text-blue-400 shrink-0 mt-0.5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
             </svg>
-            <div className="flex-1">
+            {/*<div className="flex-1">
               <p className="text-sm text-blue-200">
                 Stripe is the primary checkout. PayPal remains available as an optional fallback.
               </p>
-            </div>
+            </div>*/}
           </div>
         </div>
 
